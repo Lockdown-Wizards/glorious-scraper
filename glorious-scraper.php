2a7e161016b1f481d1afe039b193169bde70ee80
sabs21 <sabian@my.ccsu.edu>
2022-02-03 13:08:30 -0500

    added basic UI

--- glorious-scraper.php
+++ glorious-scraper.php
@@ -94,6 +94,68 @@ function setup_admin_menu(){
 }
  
 function admin_menu_init(){
-    echo "<h1>Hello World!</h1>";
+    echo "<div>
+	<h1>Event Scraper</h1>
+  </div>
+  <section>
+	<h2>Actions</h2>
+	<button>Run Scraper</button>
+  </section>
+  <section>
+	<h2>Settings</h2>
+	<form>
+	  <h3>Scheduled scrape time</h3>
+	  <div>
+		<span>
+		  <label for='hours'>Hour:</label>
+		  <select id='hours'>
+			<option value='00'>00</option>
+			<option value='01'>01</option>
+			<option value='02'>02</option>
+			<option value='03'>03</option>
+			<option value='04'>04</option>
+			<option value='05'>05</option>
+			<option value='06'>06</option>
+			<option value='07'>07</option>
+			<option value='08'>08</option>
+			<option value='09'>09</option>
+			<option value='10'>10</option>
+			<option value='11'>11</option>
+			<option value='12'>12</option>
+			<option value='13'>13</option>
+			<option value='14'>14</option>
+			<option value='15'>15</option>
+			<option value='16'>16</option>
+			<option value='17'>17</option>
+			<option value='18'>18</option>
+			<option value='19'>19</option>
+			<option value='20'>20</option>
+			<option value='21'>21</option>
+			<option value='22'>22</option>
+			<option value='23'>23</option>
+		  </select>
+		</span>
+		<span>
+		  <label for='minutes'>Minute:</label>
+		  <select id='minutes'>
+			<option value='00'>00</option>
+			<option value='05'>05</option>
+			<option value='10'>10</option>
+			<option value='15'>15</option>
+			<option value='20'>20</option>
+			<option value='25'>25</option>
+			<option value='30'>30</option>
+			<option value='35'>35</option>
+			<option value='40'>40</option>
+			<option value='45'>45</option>
+			<option value='50'>50</option>
+			<option value='55'>55</option>
+		  </select>
+		</span>
+	  </div>
+	  <br>
+	  <input type='submit' value='Save Settings'/>
+	</form>
+  </section>";
 }
 plugin_name_run();
